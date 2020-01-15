0bfa136181e447ebdca71c33d741e68719f568c7
raymondjaramillo <rjaramillo155@cnm.edu>
2020-01-15 06:41:37 -0700

    Added CharArithDemo.java

--- src/edu/cnm/deepdive/ch2/Hypot.java
+++ src/edu/cnm/deepdive/ch2/Hypot.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive.ch2;
-
+/*
+Use the Pythagorean theorem to find the length of the hypotenuse given the lengths of the two opposing sides.
+* */
 public class Hypot {
   public static void main(String args[]) {
     double x, y, z;
