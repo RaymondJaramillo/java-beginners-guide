86a597794507ab4f717cce189a18c029ce8cf045
raymondjaramillo <rjaramillo155@cnm.edu>
2020-01-14 23:19:06 -0700

    Added Hypot.java.

---
+++ src/edu/cnm/deepdive/ch2/Hypot.java
@@ -0,0 +1,15 @@
+package edu.cnm.deepdive.ch2;
+
+public class Hypot {
+  public static void main(String args[]) {
+    double x, y, z;
+
+    x = 3;
+    y = 4;
+
+    z = Math.sqrt(x * x + y * y);
+
+    System.out.println("Hypotenuse is " +z);
+  }
+
+}
