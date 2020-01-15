140f3ef213c1b835d3b2dd09577310a385a0bb7d
raymondjaramillo <rjaramillo155@cnm.edu>
2020-01-15 07:10:35 -0700

    added Sound.java and started Echo.java

---
+++ src/edu/cnm/deepdive/ch2/Echo.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive.ch2;
+
+public class Echo {
+  public static void main(String args[]) {
+    double echo;
+
+    echo = 6.3 \
+  }
+
+}
---
+++ src/edu/cnm/deepdive/ch2/Sound.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive.ch2;
+/*
+Compute kthe distance to a lightning strike whose sound takes a 7.2 seconds to reach you
+*/
+
+public class Sound {
+  public static void main(String args[]) {
+    double dist;
+
+    dist = 7.2 * 1100;
+
+    System.out.println("The lightning is  " + dist + " feet away.");
+  }
+
+
+}
