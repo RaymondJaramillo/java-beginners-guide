0e8d4f5b870ff1e769efae0a1e624f1d47f373e0
raymondjaramillo <rjaramillo155@cnm.edu>
2020-01-15 08:10:22 -0700

    Finished Echo.java

--- src/edu/cnm/deepdive/ch2/Echo.java
+++ src/edu/cnm/deepdive/ch2/Echo.java
@@ -4,7 +4,9 @@ public class Echo {
   public static void main(String args[]) {
     double echo;
 
-    echo = 6.3 \
+    echo = 2.3 * 1100 / 2;
+
+    System.out.println("The sound is " + echo + " feet away ");
   }
 
 }
